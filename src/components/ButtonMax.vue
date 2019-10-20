2743e97c00eb63c9882262bc4addc0145c123b80
Hallison Melo <hallss93@hotmail.com>
2019-10-19 21:13:12 -0300

    Button max

--- src/components/ButtonMax.vue
+++ src/components/ButtonMax.vue
@@ -1,19 +1,12 @@
 <template>
-  <button v-if="max" class="btn btn-primary scroll-btn">
+  <button class="btn btn-primary scroll-btn">
     <span>{{title}}</span>
   </button>
-  <a v-else href="#" class="btn btn-primary" data-name="seemore" name="email">
-    <span>{{title}}</span>
-  </a>
 </template>
 <script>
 export default {
   name: "KetoButtonMax",
   props: {
-    max:{
-      type: Boolean,
-      default: true
-    },
     title: {
       type: String,
       default: ""
